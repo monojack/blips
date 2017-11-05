a095f1643a48923c16f20f776493d602284e3b8a
Ionut Achim <iachim@axway.com>
2017-11-05 06:12:03 +0200

    fix(toObservable): correctly remove listeners when unsubscribing

--- src/utils/toObservable.js
+++ src/utils/toObservable.js
@@ -1,15 +1,17 @@
 import { Observable, } from 'rxjs'
 
 export const toObservable = iterator => {
-  return Observable.create(async observer => {
-    const stop = iterator.return
-    try {
-      for await (const tick of iterator) {
-        observer.next(tick)
+  return Observable.create(observer => {
+    ;(async () => {
+      try {
+        for await (const tick of iterator) {
+          observer.next(tick)
+        }
+      } catch (e) {
+        iterator.return()
+        observer.error(e)
       }
-    } catch (e) {
-      observer.error(e)
-    }
-    return stop
+    })()
+    return iterator.return
   })
 }
